57fb4a93a7e698c68110c040e8346b6f3edd2e67
FahimehS <117753512+FahimehS@users.noreply.github.com>
2023-07-12 21:26:07 +0330

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+let x =5
+ function outer () {
+
+   
+      {
+         var d = 10 ;
+         let t =3 
+      }
+      function inner () {
+         var h = 0 
+       return console.log("hello")
+      }
+      inner() ;
+      console.log("d = " , d )
+      console.log (" t =" , t)
+ }
+
+ outer();
+
+//var d = 8 
